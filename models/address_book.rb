52b978b77d5df099ae0a0fd5dc66e27a06742fc7
Melanie Eitel <me@melanieeitel.com>
2015-10-20 15:27:21 -0400

    Assignment 22 completed

--- models/address_book.rb
+++ models/address_book.rb
@@ -20,6 +20,18 @@ class AddressBook
      @entries.insert(index, Entry.new(name, phone_number, email))
    end
 
+  def iterative_search(name)
+    @entries.each do |entry|
+      if entry.name == name
+        return entry
+      end
+    end
+    
+    return nil
+  end
+
+  
+
  # Search AddressBook for a specific entry by name
    def binary_search(name)
        lower = 0
@@ -40,6 +52,7 @@ class AddressBook
          lower = mid + 1
        end
      end
+
      return nil
    end
 
